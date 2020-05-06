7f01a41b4310a5cd8393a9ab1dbd9f8000dee463
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-05-06 12:49:44 -0700

    build done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './style.css';
 import NatashaText from '../../images/home-text.png';
-import { Link } from 'react-router-dom';
 
 
 function Home() {
     console.log("hiii")
     return (
         <div className="home">
-                <img src={NatashaText} title='Click here to learn more!' className='intro' alt="Hii! I'm Natasha!" />
+                <img src={NatashaText} className='intro' alt="Hii! I'm Natasha!" />
         </div>
     )
 };
